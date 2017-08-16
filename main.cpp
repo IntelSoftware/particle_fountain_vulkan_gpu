42276f9c0fc7356fe293bc86b9afbfa3feda89f3
Tomasz Chadzynski <t.chadzynski@gmail.com>
2017-08-16 16:53:44 -0700

    Added simple command line arguments mechanism

--- main.cpp
+++ main.cpp
@@ -23,8 +23,15 @@
 
 constexpr int SCR_WIDTH = 1280;
 constexpr int SCR_HEIGHT = 960;
-constexpr std::size_t MAX_PARTICLES = 1000000;
 constexpr float WORLD_SIZE = 500;
+
+namespace {
+    std::size_t MAX_PARTICLES = 10000;
+    float RATE_NUM = 200.f;
+    float RATE_DEN = 0.1f;
+    uint32_t TTL = 600;
+};
+
 
 psim::Model::GeneratorList initGeneratorsList() 
 {
@@ -33,10 +40,10 @@ psim::Model::GeneratorList initGeneratorsList()
         { 0.0f, 250.0f, 0.0f },
         psim::generators::PointGenerator::defTheta(90, 5),
         psim::generators::PointGenerator::defPhi(-90, 5),
-        psim::generators::PointGenerator::defTTL(600, 0),
+        psim::generators::PointGenerator::defTTL(TTL, 0),
         psim::generators::PointGenerator::defMass(5.0e+10f, 0),
         psim::generators::PointGenerator::defSpeed(95, 1),
-        psim::generators::PointGenerator::defSpawningRate(200.0f, 0.1f)
+        psim::generators::PointGenerator::defSpawningRate(RATE_NUM, RATE_DEN)
     );
     genList.push_back(g);
     g = nullptr;
@@ -69,9 +76,38 @@ glm::mat4 initMVP()
     return proj * view * model;
 }
 
-
-int main()
+bool getSettings(int argc, char* argv[])
 {
+    for(int i = 1; i< argc; ++i) {
+        if(strcmp("-np", argv[i]) == 0) {
+            MAX_PARTICLES = atoi(argv[++i]);
+        } else if(strcmp("-rn", argv[i]) == 0) {
+            RATE_NUM = atof(argv[++i]);
+        } else if(strcmp("-rd", argv[i]) == 0) {
+            RATE_DEN = atof(argv[++i]);
+        } else if(strcmp("-ttl", argv[i]) == 0) {
+            TTL = atoi(argv[++i]);
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    bool success = getSettings(argc, argv);
+    if(!success) {
+    std::clog << "Incorrect arguments\n"
+            <<"\t-np <UINT>: number of particles (default: 10000)\n"
+            <<"\t-rn <FLOAT>: spawning rate #particles (default 200)\n"
+            <<"\t-rd <FLOAT>: spawning rate period in seconds(default 0.1)\n"
+            <<"\t-ttl <UINT>: particle lifetime in seconds(default 600)\n";
+            return 0;
+    }
+
     GLFWwindow *wnd;
     glfwInit();
 
